8829d5d15150c62ca31fc5246b31fb78f24ffce9
Jarryd Gerber <jgerber@tails.com>
2022-03-24 19:18:39 +0000

    test for start positions

--- parts/rotor_test.go
+++ parts/rotor_test.go
@@ -30,3 +30,28 @@ func TestRingSettings(t *testing.T) {
 		}
 	}
 }
+
+func TestStartPositions(t *testing.T) {
+
+	scenarios := []struct {
+		number        string
+		ringSetting   int
+		startPosition int
+		expect        int
+	}{
+		{number: "I", ringSetting: 0, startPosition: 0, expect: 4},
+		{number: "I", ringSetting: 0, startPosition: 1, expect: 10},
+		{number: "I", ringSetting: 0, startPosition: 2, expect: 12},
+		{number: "I", ringSetting: 0, startPosition: 3, expect: 5},
+	}
+
+	for _, scenario := range scenarios {
+		rotor := parts.CreateRotor(
+			scenario.number, scenario.startPosition, scenario.ringSetting)
+
+		if got := rotor.GetOuputPosition(); got != scenario.expect {
+			t.Errorf("Did not get expected result for input '%v'. Expected %d, got %d",
+				scenario.number, scenario.expect, got)
+		}
+	}
+}
