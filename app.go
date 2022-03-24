5f0893deb6710063f9f55cc8b58826d752571b7c
Jarryd Gerber <jgerber@tails.com>
2022-03-24 19:26:29 +0000

    start of main application

--- app.go
+++ app.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "github.com/jarryd-gerber/gonigma/parts"
 
+func main() {
+	rotorI := parts.CreateRotor("I", 0, 0)
+	rotorII := parts.CreateRotor("II", 0, 0)
+	rotorIII := parts.CreateRotor("III", 0, 0)
+
+	notch := rotorI.Rotate()
+	if notch {
+		notch = rotorII.Rotate()
+		if notch {
+			rotorIII.Rotate()
+		}
+	}
 }
