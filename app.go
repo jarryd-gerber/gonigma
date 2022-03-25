b41659bcdf4c8d2044c239fd4e90feeb6ec25e05
Jarryd Gerber <jgerber@tails.com>
2022-03-25 19:07:58 +0000

    good place. working without rotations

--- app.go
+++ app.go
@@ -13,47 +13,49 @@ func main() {
 	rotorIII, _ := parts.CreateRotor("III", 0, 0)
 	reflector, _ := parts.CreateRotor("UKW-A", 0, 0)
 
-	inputText := "E"
+	inputText := "CTSYZO"
 	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 	for _, char := range inputText {
 
-		notch := rotorIII.Rotate()
-		if notch {
-			notch = rotorII.Rotate()
-			if notch {
-				rotorI.Rotate()
-			}
-		}
+		// notch := rotorI.Rotate()
+		// if notch {
+		// 	notch = rotorII.Rotate()
+		// 	if notch {
+		// 		rotorIII.Rotate()
+		// 	}
+		// }
 
 		input := strings.Index(alphabet, string(char))
-		fmt.Printf("input is: %s\n", string(char))
+		//fmt.Printf("input is: %s\n", string(char))
 
-		inputIII := rotorIII.GetRelativeInputPosition(input)
-		pos, val := rotorIII.GetOuput(inputIII)
-		fmt.Printf("output is: %s and %d\n", val, pos)
+		inputI := rotorI.GetRelativeInputPosition(input)
+		pos, _ := rotorI.GetOuput(inputI)
+		//fmt.Printf("rotorI: %s and %d\n", val, pos)
 
 		inputII := rotorII.GetRelativeInputPosition(pos)
-		pos, val = rotorII.GetOuput(inputII)
-		fmt.Printf("output is: %s and %d\n", val, pos)
+		pos, _ = rotorII.GetOuput(inputII)
+		//fmt.Printf("rotorII: %s and %d\n", val, pos)
 
-		inputI := rotorI.GetRelativeInputPosition(pos)
-		pos, val = rotorI.GetOuput(inputI)
-		fmt.Printf("output is: %s and %d\n", val, pos)
+		inputIII := rotorIII.GetRelativeInputPosition(pos)
+		pos, _ = rotorIII.GetOuput(inputIII)
+		//fmt.Printf("rotorIII: %s and %d\n", val, pos)
 
-		pos, val = reflector.GetOuput(pos)
-		fmt.Printf("output is: %s and %d\n", val, pos)
-
-		inputI = rotorI.GetRelativeInputPosition(pos)
-		pos, val = rotorI.GetReverseOutput(inputI)
-		fmt.Printf("output is: %s and %d\n", val, pos)
-
-		inputII = rotorII.GetRelativeInputPosition(pos)
-		pos, val = rotorII.GetReverseOutput(inputII)
-		fmt.Printf("output is: %s and %d\n", val, pos)
+		pos, _ = reflector.GetOuput(pos)
+		//fmt.Printf("reflectorA: %s and %d\n", val, pos)
 
 		inputIII = rotorIII.GetRelativeInputPosition(pos)
-		_, val = rotorIII.GetReverseOutput(inputIII)
-		fmt.Printf("output is: %s and %d\n", val, pos)
+		pos, _ = rotorIII.GetReverseOutput(inputIII)
+		//fmt.Printf("rotorIII: %s and %d\n", val, pos)
+
+		inputII = rotorII.GetRelativeInputPosition(pos)
+		pos, _ = rotorII.GetReverseOutput(inputII)
+		//fmt.Printf("rotorII: %s and %d\n", val, pos)
+
+		inputI = rotorI.GetRelativeInputPosition(pos)
+		pos, _ = rotorI.GetReverseOutput(inputI)
+		//fmt.Printf("rotorI: %s and %d\n", val, pos)
+
+		fmt.Print(string(alphabet[pos]))
 	}
 }
